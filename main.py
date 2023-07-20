4ecc0999660c42d253236a16b2cb3a3eaf4d7982
ghoshdebapratim1 <89624268+ghoshdebapratim1@users.noreply.github.com>
2023-07-20 13:49:58 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+#import libraries
+import streamlit as st
+import pandas as pd
+#import matplotlib.pyplot as plt
+#import numpy as np
+import plotly.figure_factory as ff
+
+#look for more information here https://docs.streamlit.io/library/cheatsheet
+
+#adding title
+st.title("Hungry Hashbrowns - Spotify Dataset")
