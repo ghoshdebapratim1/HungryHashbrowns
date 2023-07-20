4deb8a5eea0df34cdffb5be0af86d293220c30b0
ghoshdebapratim1 <89624268+ghoshdebapratim1@users.noreply.github.com>
2023-07-20 13:52:11 +0000

    First Commit 20230720

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 import pandas as pd
 #import matplotlib.pyplot as plt
 #import numpy as np
-import plotly.figure_factory as ff
+import plotly.express as px
 
 #look for more information here https://docs.streamlit.io/library/cheatsheet
 
